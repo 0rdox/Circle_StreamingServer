622b8f7808627494d1a929c293cbaa9e31b1a1d4
J.Seremak <janko2002@outlook.com>
2024-06-12 14:03:59 +0200

    Streamer start stream, before new method

--- SocketIOServer/server.js
+++ SocketIOServer/server.js
@@ -1,5 +1,9 @@
 const { Server } = require("socket.io");
+const fs = require("fs");
+const path = require("path");
+const { Stream } = require("stream");
 
+const streams = {};
 
 const io = new Server(3000, {
   cors: {
@@ -8,29 +12,83 @@ const io = new Server(3000, {
   }
 });
 
+map = new Map();
+
 
 io.on("connection", (socket) => {
-
-
   console.log(`connect ${socket.id}`);
 
+
+
+
+
+
+  //Receiving data from streamer
   socket.on("stream", (streamObject) => {
-    //Create room 
-    console.log("Started room: " + streamObject.userId)
-    // const userId = streamObject.userId;
-    // io.emit("stream", streamObject);
+    const userId = streamObject.userId;
 
-    io.emit("stream", streamObject);
+    //Check if streamObject contains userId
+    if (!userId) {
+      console.error('Received stream object without userId:', streamObject);
+      return;
+    }
 
 
-    //Dont use to, because it will only send to the user that is connected to the server instead of all users connected to the room
-    // io.in(userId).emit("stream", streamObject);
+    //Check if user has a stream array, if not; create it.
+    if (!streams[userId]) {
+      console.log("reached")
+      streams[userId] = [];
+
+      console.log("Created room with id: " + userId);
+    }
+
+
+    streams[userId].push(streamObject);
+    console.log(streams[userId].length);
+    //How to continue streaming to the room?
+    io.in(userId).emit('stream', streamObject);
   });
+
+  // Joining a room
+  socket.on('joinRoom', (StreamName) => {
+    socket.join(StreamName);
+
+
+
+
+    map.push(StreamName, socket.id);
+
+
+    console.log(`User ${socket.id} joined room ${StreamName}`);
+
+    // Send all previously saved streams to the new client
+    if (streams[StreamName]) {
+
+      // console.log("Getting array", streams[userId]);
+      streams[StreamName].forEach((streamObject) => {
+        socket.emit('stream', streamObject);
+      });
+    }
+
+  });
+
+
+
 
   socket.on("disconnect", () => {
     console.log(`disconnect ${socket.id}`);
+
+
+
+    //If the streamer disconnects, delete the cached stream
+
+    if (streams[roomId]) {
+      delete streams[roomId];
+    }
+
   });
 });
+
 
 console.log("Server started at ws://localhost:3000/");
 
