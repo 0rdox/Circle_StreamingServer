c2352bcbc4e02d252499d60039c0f009a3934ac5
J.Seremak <janko2002@outlook.com>
2024-06-13 11:17:25 +0200

    Works with Streamer

--- SocketIOServer/server.js
+++ SocketIOServer/server.js
@@ -15,11 +15,10 @@ const io = new Server(3000, {
 
 // Je moet ervoor zorgen dat alleen streamers iets kunnen sturen.
 
-
 io.on("connection", (socket) => {
-  // const token = socket.handshake.headers.authorization;
+  //Check if user is valid
+  //const token = socket.handshake.headers.authorization;
 
-  //Check if token is valid
   // jwt.verify token
   // map (socketId, [verified token userId, "viewer"])   //when user starts streaming, change the unknown to streamer
 
@@ -29,22 +28,14 @@ io.on("connection", (socket) => {
   socket.on("stream", (streamObject) => {
     const userId = streamObject.userId;
 
-    console.log("Received stream object:", streamObject);
     //Check if streamObject contains userId
     if (!userId) {
       console.error('Received stream object without userId:', streamObject);
       return;
     }
 
-    //map socket id to user id (maybe use socket.id + role?)
-    //map with 3 parameters ? (socket + userId is the same in streamer and viewer because the roomcode is user id of the streamer. if you do [userId (roomcode)] and [socket.id + role (viewer/streamer)]
-    // you can check in onDisconnect for the socket.id, if socketId is a streamer, you can check the userId associated with the socketId and delete the stream.
-    //I can also have 2 maps, one for the socketId and one for the userId. I can check if the socketId is a streamer or viewer and then check the userId associated with the socketId and delete the stream.
-
     //Map.push (socketId), [(roomId), (userRole)]
     map.set(socket.id, [streamObject.userId, "Streamer"]);
-
-
 
     //Check if user has a stream array, if not; create it.
     if (!streams[userId]) {
@@ -56,15 +47,15 @@ io.on("connection", (socket) => {
 
 
     streams[userId].push(streamObject);
-    console.log(streams[userId].length);
     //How to continue streaming to the room?
-    io.in(userId).emit('stream', streamObject);
+    io.in(userId).emit("stream", streamObject);
+    // io.emit("stream", streamObject);
   });
 
   // Joining a room
   socket.on('joinRoom', (RoomId) => {
     socket.join(RoomId);
-    // console.log(socket.id + " Joined room: " + RoomId);
+    console.log(socket.id + " Joined room: " + RoomId);
 
     //Map socket id to room id
     map.set(socket.id, [RoomId, "Viewer"]);
@@ -72,7 +63,7 @@ io.on("connection", (socket) => {
 
     // Send all previously saved streams to the new client
     if (streams[RoomId]) {
-      // console.log("Getting array", streams[userId]);
+      console.log("Getting array", streams[RoomId]);
       streams[RoomId].forEach((streamObject) => {
         socket.emit('stream', streamObject);
       });
@@ -82,33 +73,27 @@ io.on("connection", (socket) => {
 
 
 
-  socket.on("stopStream", (streamObject) => {
-    if (streams[roomId]) {
-      delete streams[roomId];
-    }
-  })
+  // socket.on("stopStream", (streamObject) => {
+  //   if (streams[roomId]) {
+  //     delete streams[roomId];
+  //   }
+  // })
 
 
   socket.on("disconnect", () => {
     console.log(`disconnect ${socket.id}`);
 
-    //Uit socket header de user krijgen
-
-
-    //check socketId with userId
-    // check userId with existing streams
-
-    // if stream exist delete stream
-
-    //If the streamer disconnects, delete the cached stream
-
-
-    console.log(map.get(socket.id));
-
-
-    // if (streams[roomId]) {
-    //   delete streams[roomId];
-    // }
+    //Checks if the socket is a streamer and deletes the stream if it is.
+    const user = map.get(socket.id);
+    if (user && user[1] === "Streamer") {
+      const roomId = user[0];
+      if (streams[roomId]) {
+        delete streams[roomId];
+        console.log("Stream Deleted");
+      }
+    } else {
+      console.log("User not added")
+    }
 
   });
 });
